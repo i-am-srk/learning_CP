e089dfae876ee707226e1377f02b487be4e9b3b5
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2025-06-29 11:04:43 +0530

    leetcode - 26

--- practice/demo.py
+++ practice/demo.py
@@ -9,23 +9,32 @@ def get_ints():
 def write_output(s):
     sys.stdout.write(str(s)+'\n')
 
-def check(nums: list[int]) -> bool:
-    n=len(nums)
-    if n <= 1:
-        return True
-    
-    inversion_count=0
-    for i in range(1,n):
-        if nums[i] < nums[i-1]:
-            inversion_count+=1
+def removeDuplicates(nums: list[int]) -> int:
+    n = len(nums)
+    if n<=1:
+        return 1
+    l = 0
+    for r in range(1, n):
+        if nums[r]==nums[l]:
+            if l-1>=0 and nums[r]==nums[l-1]:
+                r+=1
+            else:
+                l+=1
+                r+=1
+        else:
+            if l-1>=0 and nums[r]==nums[l-1]:
+                r+=1
+            elif l-1>=0 and nums[r]!=nums[l-1]:
+                nums[r], nums[l] = nums[l], nums[r]
+                l+=1
+                r+=1
+            else:
+                l+=2
+                r+=2
 
-        if inversion_count > 1:
-            return False
-        
-    if nums[0] < nums[n-1]:
-        inversion_count+=1
+    return l
 
-    return inversion_count <= 1
 
-nums = get_ints()
-write_output(check(nums))
+nums=get_ints()
+write_output(removeDuplicates(nums))
+write_output(nums)
