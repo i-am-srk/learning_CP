5fee4424de26978f7d0dd2bfe51837ceb34cea00
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2025-07-06 12:26:12 +0530

    leetcode 283

--- practice/demo.py
+++ practice/demo.py
@@ -9,32 +9,26 @@ def get_ints():
 def write_output(s):
     sys.stdout.write(str(s)+'\n')
 
-def removeDuplicates(nums: list[int]) -> int:
-    n = len(nums)
-    if n<=1:
-        return 1
-    l = 0
-    for r in range(1, n):
-        if nums[r]==nums[l]:
-            if l-1>=0 and nums[r]==nums[l-1]:
-                r+=1
-            else:
-                l+=1
-                r+=1
-        else:
-            if l-1>=0 and nums[r]==nums[l-1]:
-                r+=1
-            elif l-1>=0 and nums[r]!=nums[l-1]:
-                nums[r], nums[l] = nums[l], nums[r]
-                l+=1
-                r+=1
-            else:
-                l+=2
-                r+=2
+def rotateArray(nums: list[int], start: int, end: int):
+    n = end-start
+    k = n-1
+    nums[start:end] = reversed(nums[start:end])
+    nums[start:start+k] = reversed(nums[start:start+k])
+    nums[start+k:end] = reversed(nums[start+k:end])
 
-    return l
+def moveZeros(nums: list[int]):
+    n = len(nums)
+    zeros_count = 0
+    for i in range(n-1, -1, -1):
+        if nums[i]!=0:
+            continue
+        else:
+            start = i
+            end = n-zeros_count
+            rotateArray(nums, start, end)
+            zeros_count+=1
 
 
 nums=get_ints()
-write_output(removeDuplicates(nums))
+moveZeros(nums)
 write_output(nums)
