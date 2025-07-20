f7f4ff3cf2989842f20a3895301b6754d3c24404
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2025-07-21 01:54:48 +0530

    leetcode 268 - T:O(N) | S:O(N)

--- practice/demo.py
+++ practice/demo.py
@@ -9,46 +9,23 @@ def get_ints():
 def write_output(s):
     sys.stdout.write(str(s)+'\n')
 
-def unionArray(nums1: list[int], nums2: list[int]) -> list[int]:
-    i, j = 0, 0
-    union_array = []
+def missingNumber(nums: list[int]) -> int:
+    n=len(nums)
+    visited=[False]*(n+1)
+    
+    for num in nums:
+        visited[num]=True
 
-    while i<len(nums1) and j<len(nums2):
-        if nums1[i]<nums2[j]:
-            num=nums1[i]
-            i+=1
-        elif nums1[i]>nums2[j]:
-            num=nums2[j]
-            j+=1
-        else:
-            num=nums1[i]
-            i+=1
-            j+=1
-
-        if len(union_array)>0 and union_array[-1]==num:
+    ans=-1
+    for i in range(len(visited)):
+        if visited[i]:
             continue
-        else:
-            union_array.append(num)
 
-    while i<len(nums1):
-        num=nums1[i]
-        i+=1
-        if len(union_array)>0 and union_array[-1]==num:
-            continue
-        else:
-            union_array.append(num)
+        ans = i
+        break
 
-    while j<len(nums2):
-        num=nums2[j]
-        j+=1
-        if len(union_array)>0 and union_array[-1]==num:
-            continue
-        else:
-            union_array.append(num)
+    return ans
 
-    return union_array
-
-nums1=get_ints()
-nums2=get_ints()
-result = unionArray(nums1, nums2)
+nums=get_ints()
+result = missingNumber(nums)
 write_output(result)
