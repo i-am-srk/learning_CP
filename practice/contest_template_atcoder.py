1e6ec0c2103889f8e3d8f3bf532d06419af5c1d0
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2025-10-18 19:03:52 +0530

    ATCODER: O(nlogn)T: O(n)S: Most Frequest Substrings

--- practice/contest_template_atcoder.py
+++ practice/contest_template_atcoder.py
@@ -1,11 +1,28 @@
 import sys
+from collections import defaultdict
 
-inputs = list(map(int, sys.stdin.readline().strip().split()))
+n, k = list(map(int, sys.stdin.readline().strip().split()))
 
-s, a, b, x = inputs
+s = sys.stdin.readline().strip()
 
-total_distance = 0
+# generate and count all substrings
+counts: dict[str, int] = defaultdict(int)
+for i in range(n-k+1):
+    substring = s[i:i+k]
+    counts[substring]+=1
 
-total_distance += ((x//(a+b))*(s*a)) + (((x%(a+b))*s) if (x%(a+b)) < a else (s*a))
+# find max occurence
+max_occurence: int = 0
+if counts:
+    max_occurence = max(counts.values())
 
-print(total_distance)
+# filter and output
+max_strings: list[str] = []
+for sub, count in counts.items():
+    if count == max_occurence:
+        max_strings.append(sub)
+
+max_strings.sort()
+
+sys.stdout.write(str(max_occurence)+'\n')
+sys.stdout.write(' '.join(max_strings)+'\n')
