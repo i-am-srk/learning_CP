d8a713a695c43a62c0618e3485511997bb3814fe
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2025-11-02 09:11:36 +0530

    LC-contest: O(nlogn)T: O(1)S

--- practice/contest_template_leetcode.py
+++ practice/contest_template_leetcode.py
@@ -1,35 +1,23 @@
 import sys
 
-def solve(nums: list[int]) -> int:
+def solve(nums: list[int]) -> list[int]:
     n=len(nums)
-    if n==0:
-        return 0
+    nums.sort()
+
+    l, r = nums[0], nums[n-1]
+    ans = []
+    i = 0
+
+    while l<=r and i<n:
+        if nums[i]==l:
+            i+=1
+            l+=1
+            continue
+        ans.append(l)
+        l+=1
+
+    return ans
     
-    tavernilo = nums
-    max_length=0
-
-    for i in range(n):
-        distinct_evens = set()
-        distinct_odds = set()
-
-        for j in range(i, n):
-            current_num = tavernilo[j]
-            if current_num%2==0:
-                distinct_evens.add(current_num)
-            else:
-                distinct_odds.add(current_num)
-
-            count_evens = len(distinct_evens)
-            count_odds = len(distinct_odds)
-
-            if count_evens==count_odds:
-                current_length=j-i+1
-                max_length=max(max_length, current_length)
-
-    return max_length
-    
-
-
 nums = list(map(int, sys.stdin.readline().strip().split()))
 # k = int(sys.stdin.readline().strip())
 ans = solve(nums)
