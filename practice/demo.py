76527dacfefb06ded26a3a1652e41445d1ce1831
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2025-11-03 16:41:31 +0530

    NC: O(N)T: O(1)S: using hash map

--- practice/demo.py
+++ practice/demo.py
@@ -9,22 +9,35 @@ def get_ints():
 def write_output(s):
     sys.stdout.write(str(s)+'\n')
 
-def subarraySum(nums: list[int], k: int) -> int:
-    count = 0
-    current_sum = 0
-    prefix_sum = {}
-    prefix_sum[0]=1
+def isAnagram(s: str, t:str) -> bool:
+    if len(s)!=len(t):
+        return False
+    
+    s_freq = {}
+    t_freq = {}
 
-    for num in nums:
-        current_sum+=num
-        if current_sum-k in prefix_sum:
-            count+=prefix_sum[current_sum-k]
-        prefix_sum[current_sum]=prefix_sum.get(current_sum, 0)+1
+    for c in s:
+        if c in s_freq:
+            s_freq[c]+=1
+        else:
+            s_freq[c]=1
 
-    return count
+    for c in t:
+        if c in t_freq:
+            t_freq[c]+=1
+        else:
+            t_freq[c]=1
+
+    for key, value in s_freq.items():
+        if key not in t_freq or t_freq[key]!=value:
+            return False
+        
+    return True
 
 
-nums=get_ints()
-k=int(get_line())
-result = subarraySum(nums, k)
+str_1 = get_line()
+str_2 = get_line()
+# nums=get_ints()
+# k=int(get_line())
+result = isAnagram(str_1, str_2)
 write_output(result)
