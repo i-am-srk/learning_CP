66b905162e12f0468e39bfb96c98e65e1a522e27
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2026-01-12 21:11:39 +0530

    cf-1072a

--- practice/contest_template_cf.py
+++ practice/contest_template_cf.py
@@ -9,31 +9,28 @@ def get_list_input():
 def output(s):
     return sys.stdout.write(str(s)+"\n")
 
-def solve(n, k):
-    solved = 0
-    total_time = 240
-    available_time = total_time-k
-    for i in range(1, n+1):
-        available_time -= 5*i
-        if available_time >= 0:
-            solved+=1
-
-    return solved
+def solve(n):
+    if n==2:
+        return 2
+    elif n==3:
+        return 3
+    else:
+        return n%2
 
 def cf():
-    n, k = get_list_input()
-    answer = solve(n, k)
-    output(answer)
+    # n, k = get_list_input()
+    # answer = solve(n, k)
+    # output(answer)
 
 
-    # t = int(get_line())
+    t = int(get_line())
 
-    # while t:
-    #     sticks = get_list_input()
-    #     answer = "YES" if check_square(sticks) else "NO"
-    #     output(answer)
+    while t:
+        people = int(get_line())
+        answer = solve(people)
+        output(answer)
 
-    #     t-=1
+        t-=1
 
 if __name__ == "__main__":
     cf()
