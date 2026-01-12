fae64b61f12df8552a4c9a54337ab74871d14cef
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2026-01-12 21:50:34 +0530

    cf-1072c

--- practice/contest_template_cf.py
+++ practice/contest_template_cf.py
@@ -9,19 +9,24 @@ def get_list_input():
 def output(s):
     return sys.stdout.write(str(s)+"\n")
 
-def solve(s, k, m):
-    num_flips = m//k
-    time_left = m%k
+def solve(n, k):
+    if k>n:
+        return -1
+    
+    found=False
+    for d in range(32):
+        low=n//(1<<d)
+        high=(n+(1<<d)-1)//(1<<d)
 
-    if num_flips==0:
-        sand_left=s
-    elif num_flips%2==1:
-        sand_left=min(s,k)
-    else:
-        sand_left=s
-
-    rem_sand = max(0, sand_left - time_left)
-    return rem_sand
+        if low==k or high==k:
+            found=True
+            return d
+        
+        if high<k:
+            break
+    
+    if not found:
+        return -1
 
 def cf():
     # n, k = get_list_input()
@@ -32,8 +37,8 @@ def cf():
     t = int(get_line())
 
     while t:
-        s, k, m = get_list_input()
-        answer = solve(s, k, m)
+        n, k = get_list_input()
+        answer = solve(n, k)
         output(answer)
 
         t-=1
