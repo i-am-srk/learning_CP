f5c7dfcdae603cf3667363573881709194e11be3
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2026-01-12 22:14:17 +0530

    cf-1072e

--- practice/contest_template_cf.py
+++ practice/contest_template_cf.py
@@ -9,27 +9,45 @@ def get_list_input():
 def output(s):
     return sys.stdout.write(str(s)+"\n")
 
-MAX_D=32
-C= [[0]*MAX_D for _ in range (MAX_D)]
-for i in range (MAX_D):
-    C[i][0]=1
-    for j in range (1,i+1):
-        C[i][j]=C[i-1][j-1]+C[i-1][j]
+def solve(n, p):
+    if n<2:
+        return 0
+    
+    d = [abs(p[i] - p[i+1]) for i in range(n-1)]
+    m = n - 1
+    
+    left = [-1] * m
+    right = [m] * m
+    stack = []
+    
+    for i in range(m):
+        while stack and d[stack[-1]] >= d[i]:
+            stack.pop()
+        if stack:
+            left[i] = stack[-1]
+        stack.append(i)
+        
+    stack = []
+    for i in range(m-1, -1, -1):
+        while stack and d[stack[-1]] > d[i]:
+            stack.pop()
+        if stack:
+            right[i] = stack[-1]
+        stack.append(i)
 
-def solve(n, k):
-    d=n.bit_length()-1
-    ans=0
+    freq = [0] * (n + 1)
+    for i in range(m):
+        count = (i - left[i]) * (right[i] - i)
+        freq[d[i]] += count
 
-    if d+1>k:
-        ans+=1
+    ans = [0] * (n + 1)
+    current_sum = 0
+    for k in range(n - 1, 0, -1):
+        current_sum += freq[k]
+        ans[k] = current_sum
 
-    for p in range(d):
-        target_c= k-p
+    return " ".join(map(str, ans[1:n]))
 
-        for c in range(max(0, target_c), p+1):
-            ans+=C[p][c]
-
-    return ans
 
 def cf():
     # n, k = get_list_input()
@@ -40,8 +58,9 @@ def cf():
     t = int(get_line())
 
     while t:
-        n, k = get_list_input()
-        answer = solve(n, k)
+        n = int(get_line())
+        p = get_list_input()
+        answer = solve(n, p)
         output(answer)
 
         t-=1
